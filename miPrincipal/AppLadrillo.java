e9a93d423c4d1c12628c7c50030b65bbc056b282
Osvaldo Rodriguez Armenta <158524993+OsvaldoRA@users.noreply.github.com>
2024-02-26 16:45:28 +0000

    Latas

--- miPrincipal/AppLadrillo.java
+++ miPrincipal/AppLadrillo.java
@@ -5,7 +5,7 @@ public class AppLadrillo {
         System.out.println("Version Iterativa ");
         dibujar_pared_ite(10);
         System.out.println("Version Recursiva ");
-        dibujar_pared_rec(10);
+        dibujar_pared_rec(1);
     }
     public static void dibujar_pared_ite(int n){
         for(int i=0;i<n;i++){
@@ -15,10 +15,8 @@ public class AppLadrillo {
 
     }
     public static void dibujar_pared_rec(int n){
-        if(n>0)
-        dibujar_pared_rec(n-1);
-        
-        
+        if(n<10)
+        dibujar_pared_rec(n+1);
         System.out.println("XXXXXXXXXXXXXXXXXXXXX");
     }
 }
