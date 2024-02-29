dd8f6c98452dfd8a02222754502bc5a7c2e1afdb
Osvaldo Rodriguez Armenta <158524993+OsvaldoRA@users.noreply.github.com>
2024-02-29 01:45:22 +0000

    Listo

--- miPrincipal/Alfabeto.java
+++ miPrincipal/Alfabeto.java
@@ -1,20 +1,18 @@
 package miPrincipal;
 public class Alfabeto {
 
-    public static void menu() {
-        System.out.println(" ");
-        metodo ('Z');
-        System.out.println(" ");
-
-    
-        
+    public static void menu(){
+        System.out.println("");
+        metodoA('Z');
+        System.out.println("");
     }
     static void metodoA(char c){
-        if(c>'A')
-        metodo(c);
+        if(c > 'A')
+            metodoB(c);
         System.out.println(c);
+
     }
     static void metodoB(char c){
-        metodo(--c);
-    } 
+        metodoA(--c);
+    }
 }
--- miPrincipal/MaximoComunDivisor.java
+++ miPrincipal/MaximoComunDivisor.java
@@ -1,21 +1,16 @@
 package miPrincipal;
 public class MaximoComunDivisor {
 
-    public static void menu() {
-        System.out.println(mcd (6,124));
-        System.out.println(mcd (8,64));
-
-        
-    }
-    public static int mcd (int m, int n) {
-        if(n<=m && m%n ==0){
+    public static void menu(){
+        System.out.println(mcd(6,124));
+        System.out.println(mcd(9,81));
+    }    
+    public static int mcd(int m, int n){
+        if(n <= m && m % n == 0)
             return n;
-        }
-        else if(m<n){
+        else if(m < n)
             return mcd(n,m);
-        }
         else
-        return mcd(m, n);
-        
+            return mcd(n, m%n);
     }
 }
